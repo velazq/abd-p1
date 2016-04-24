58927da33d20861c19c73b16d5c247c6d5c57192
A V <netnoc@gmail.com>
2016-04-24 21:02:19 +0200

    Distancia entre usuarios

--- practica1User701/src/main/java/abd/p1/dao/UsuarioDAOImpl.java
+++ practica1User701/src/main/java/abd/p1/dao/UsuarioDAOImpl.java
@@ -1,5 +1,8 @@
 package abd.p1.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -19,10 +22,11 @@ public class UsuarioDAOImpl extends GenericDAOImpl<Usuario, Integer> implements 
 	}
 	
 	public Usuario login(String loginName, String passwd) {
+		String hql = "from Usuario as u where u.email = :email";
 		Usuario usuario = null;
 		try {
 			Session s = begin();
-			Query q = s.createQuery("from usuario as u where u.email = :email");
+			Query q = s.createQuery(hql);
 			q.setString("email", loginName);
 			Usuario usr = (Usuario) q.uniqueResult();
 			if (usr != null && usr.getContrasena() == passwd) {
@@ -34,6 +38,31 @@ public class UsuarioDAOImpl extends GenericDAOImpl<Usuario, Integer> implements 
 			e.printStackTrace();
 		}
 		return usuario;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Usuario> nearestUsers(Usuario usr, String nameFilter, int limit) {
+		String hql = "from Usuario as u ";
+		if (nameFilter != null)
+			hql += "where lower(u.nombre) like :nameFilter ";
+		hql += "order by power(u.latitud - :latitud, 2) + power(u.longitud - :longitud, 2)";
+		List<Usuario> usrs = null;
+		try {
+			Session s = begin();
+			Query q = s.createQuery(hql);
+			q.setDouble("latitud", usr.getLatitud());
+			q.setDouble("longitud", usr.getLongitud());
+			if (nameFilter != null)
+				q.setString("nameFilter", '%' + nameFilter.toLowerCase() + '%');
+			if (limit > 0)
+				q.setMaxResults(limit);
+			usrs = (List<Usuario>) q.list();
+			commit();
+		} catch (Exception e) {
+			rollback();
+			e.printStackTrace();
+		}
+		return usrs;
 	}
 
 }
