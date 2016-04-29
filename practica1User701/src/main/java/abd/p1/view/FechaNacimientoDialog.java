8b7b6eea25ae25c34991ed2762c3ef4ad7642856
A V <netnoc@gmail.com>
2016-04-29 12:53:36 +0200

    nombre de FechaNacimientoDialog

--- practica1User701/src/main/java/abd/p1/view/FechaNacimientoDialog.java
+++ practica1User701/src/main/java/abd/p1/view/FechaNacimientoDialog.java
@@ -12,12 +12,12 @@ import java.util.Date;
  *
  * @author Guilherme
  */
-public class fechaNacimientoDialog extends javax.swing.JDialog {
+public class FechaNacimientoDialog extends javax.swing.JDialog {
 
     /**
      * Creates new form fechaNacimientoDialog
      */
-    public fechaNacimientoDialog(java.awt.Frame parent, boolean modal) {
+    public FechaNacimientoDialog(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
     }
@@ -116,20 +116,20 @@ public class fechaNacimientoDialog extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(fechaNacimientoDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FechaNacimientoDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(fechaNacimientoDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FechaNacimientoDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(fechaNacimientoDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FechaNacimientoDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(fechaNacimientoDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FechaNacimientoDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                fechaNacimientoDialog dialog = new fechaNacimientoDialog(new javax.swing.JFrame(), true);
+                FechaNacimientoDialog dialog = new FechaNacimientoDialog(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
